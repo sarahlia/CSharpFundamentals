8f7faa30768f8b9a5db70e613c0b1b23b6740334
sarahlia <sarahlia23@outlook.com>
2020-11-29 16:06:22 -0600

    finish switch statement.

--- Fundamentals-PS/Book.cs
+++ Fundamentals-PS/Book.cs
@@ -11,6 +11,28 @@ namespace Fundamentals_PS
             Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
             if(grade <= 100 && grade >= 0)
--- Fundamentals-PS/Program.cs
+++ Fundamentals-PS/Program.cs
@@ -11,13 +11,14 @@ namespace Fundamentals_PS
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
+            book.AddLetterGrade('C');
 
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The highest grade is {stats.High:N1}");
             Console.WriteLine($"The lowest grade is {stats.Low:N1}");
             Console.WriteLine($"The total grade is {stats.Total:N2}");
-            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The average grade is {stats.Average:N2}");
         }
     }
 }
