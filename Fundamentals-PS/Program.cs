aea22be6c42de1c1eb9177b8035427955d319641
sarahlia <sarahn@rightnowmedia.org>
2021-04-19 16:27:02 -0500

    finish hello.

--- Fundamentals-PS/Program.cs
+++ Fundamentals-PS/Program.cs
@@ -7,6 +7,12 @@ namespace Fundamentals_PS
     {
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            Console.WriteLine("Hello.");
+            
             var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
 
             var total = 0.0;
