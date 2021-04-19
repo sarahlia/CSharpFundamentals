d5e7e143795b9262dded734c4c14adadb492f9b7
sarahlia <sarahn@rightnowmedia.org>
2021-04-19 16:24:19 -0500

    finish syntax.

--- Fundamentals-PS/Program.cs
+++ Fundamentals-PS/Program.cs
@@ -7,6 +7,32 @@ namespace Fundamentals_PS
     {
         static void Main(string[] args)
         {
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+
+            var total = 0.0;
+            foreach (var number in numbers)
+            {
+                total += number;
+            }
+            Console.WriteLine($"The total is {total}");
+
+            var grades = new List<double>
+            {
+                12.7,
+                10.3,
+                6.11,
+                4.1,
+                56.1
+            };
+
+            var result = 0.0;
+            foreach (var grade in grades)
+            {
+                result += grade;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average is {result:N3}");
+            
             IBook book = new DiskBook("Sarah's Grade Book");
             Console.WriteLine(book.Name);
 
